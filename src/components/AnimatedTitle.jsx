d50cd090a6774dfdf4d4c87b01723faa39ff8537
rvn14 <dasunadithya123@gmail.com>
2025-08-11 05:26:48 +0530

    updated

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -8,55 +8,59 @@ gsap.registerPlugin(ScrollTrigger);
 
 const AnimatedTitle = ({ title, containerClass }) => {
   const containerRef = useRef(null);
+  const animatedWords = useRef([]); // Will hold arrays of refs per line
 
-  useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      const titleAnimation = gsap.timeline({
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: "100 bottom",
-          end: "center bottom",
-          toggleActions: "play none none reverse",
-          markers: true,
-        },
-      });
+useLayoutEffect(() => {
+  const allWords = animatedWords.current.flat();
+  const ctx = gsap.context(() => {
+    const titleAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "10% bottom",
+        toggleActions: "play none none reverse",
+        markers: true,
+      },
+    });
 
-      titleAnimation.to(
-        ".animated-word",
-        {
-          opacity: 1,
-          transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
-          duration: 1,
-          ease: "power2.inOut",
-          stagger: 0.02,
-        },
-        0
-      );
-      // Refresh ScrollTrigger after animation setup
-      ScrollTrigger.refresh();
-    }, containerRef);
+    titleAnimation.to(
+      allWords,
+      {
+        opacity: 1,
+        transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
+        duration: 1,
+        ease: "power2.inOut",
+        stagger: 0.02,
+      },
+      0
+    );
+    ScrollTrigger.refresh();
+  }, containerRef);
 
-    return () => ctx.revert();
-  }, []);
-  return (
-    <div ref={containerRef} className={clsx("animated-title", containerClass)}>
-      {title.split("<br />").map((line, index) => (
-        <div
-          key={index}
-          className="flex-center max-w-full flex-wrap gap-2 px-8 md:gap-3"
-        >
-          {line.split(" ").map((word, idx) => (
-            <span
-              key={idx}
-              className="animated-word font-light"
-              dangerouslySetInnerHTML={{ __html: word }}
-            />
-          ))}
-        </div>
-      ))}
-    </div>
-  );
-};
+  return () => ctx.revert();
+}, []);
+
+return (
+  <div ref={containerRef} className={clsx("animated-title", containerClass)}>
+    {title.split("<br />").map((line, lineIdx) => (
+      <div
+        key={lineIdx}
+        className="flex-center max-w-full flex-wrap gap-2 px-8 md:gap-3"
+      >
+        {line.split(" ").map((word, wordIdx) => (
+          <span
+            ref={el => {
+              if (!animatedWords.current[lineIdx]) animatedWords.current[lineIdx] = [];
+              animatedWords.current[lineIdx][wordIdx] = el;
+            }}
+            key={wordIdx}
+            className="animated-word font-light"
+            dangerouslySetInnerHTML={{ __html: word }}
+          />
+        ))}
+      </div>
+    ))}
+  </div>
+)};
 AnimatedTitle.propTypes = {
   title: PropTypes.string.isRequired,
   containerClass: PropTypes.string,
